f08ccc08d716d9ce08bf27f8bab702b21e9ccf95
randumduck <100600785+randumduck@users.noreply.github.com>
2024-12-18 19:52:08 +0530

    Update setup_vuln_webservers.sh

--- setup_vuln_webservers.sh
+++ setup_vuln_webservers.sh
@@ -50,9 +50,9 @@ sudo cp db.vuln.test /etc/bind/
 # Restart bind9 service
 sudo systemctl restart bind9
 
-# Create Docker containers with vulnerable web servers
+# Create Docker containers with vulnerable web servers and expose ports
 for i in {1..5}; do
-    sudo docker run -d --name web$i --network vuln_network --ip 10.0.0.$((i+2)) -h web$i.vuln.test -v /var/www/html/web$i:/usr/local/apache2/htdocs/ --memory="256m" --cpus="0.5" httpd:2.4.29
+    sudo docker run -d --name web$i --network vuln_network --ip 10.0.0.$((i+2)) -h web$i.vuln.test -v /var/www/html/web$i:/usr/local/apache2/htdocs/ --memory="256m" --cpus="0.5" -p $((8080+i)):80 httpd:2.4.29
 done
 
 # Create index.html for each web server
@@ -82,7 +82,7 @@ Web Server Details:
 EOF
 
 for i in {1..5}; do
-    echo "web$i.vuln.test - 10.0.0.$((i+2)) - Apache 2.4.29 - Docker Image: httpd:2.4.29" >> ~/Desktop/web_servers_info.txt
+    echo "web$i.vuln.test - 10.0.0.$((i+2)) - Apache 2.4.29 - Docker Image: httpd:2.4.29 - Host Port: $((8080+i))" >> ~/Desktop/web_servers_info.txt
 done
 
 echo "Setup complete. Web server details saved to ~/Desktop/web_servers_info.txt"
